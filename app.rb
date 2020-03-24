e71aeb3221b974251058e605a8de17311622a373
Russell Macalintal <russell.macalintal@gmail.com>
2020-03-24 05:39:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,8 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @product = (params[:num1].to_i)*(params[:num2].to_i)
-    "#{@product}"
+    # @product = (params[:num1].to_i)*(params[:num2].to_i)
+    # "#{@product}"
+    "#{(params[:num1].to_i)*(params[:num2].to_i)}"
   end
 end
